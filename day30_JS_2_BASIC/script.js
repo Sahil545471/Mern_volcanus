c537e778bbd2529ae502e03b307d5f357bc3a7fb
sahil545471 <c.sahil545471@gmail.com>
2024-02-06 12:59:20 +0530

    javascript besic

---
+++ day30_JS_2_BASIC/script.js
@@ -0,0 +1,63 @@
+// console.log("hello world")
+// // {2+5}
+
+// let number1 = 10
+// let number2 = 100
+// let ram = 20
+
+//  console.log(number1+number2);
+//  console.log(ram);
+
+// let number = 30.2
+
+// console.log(number)
+
+// let char = 's'
+// console.log(char);
+
+// let Name = "superman"
+// console.log(Name)
+
+// let is18 = false
+
+// console.log(is18)
+
+// let $dollar = 3000
+// console.log("i have ",$dollar,"$")
+
+// let = name1 = "superman"
+// let = name2 = "spiderman"
+
+// console.log(name1,"belong to marver",name2,"belongs to dc" )
+// console.log('"'+name1+'"',"belong to marver ",name2,"belongs to dc" )
+
+// let = name1 = "superman"
+// let = name2 = "spiderman"
+
+// console.log(`"${name1}" is belong from 'dc' and "${name2}" is belong from "marvel"`)
+
+// name1 = "aquaman"
+
+// console.log(`"${name1}" is belong from 'dc' and "${name2}" is belong from "marvel"`)
+
+
+// You cannot change the value constant 
+// const number = 220
+// number = 30
+// console.log(number)
+
+// let number1 = 20
+// let number2 = 30
+
+// console.log(++number1);
+
+// const number = "20";
+
+// It checks the value only
+// console.log(number == 20)
+
+// It checks the value and type both
+// console.log(number === 20)
+
+// console.log(typeof(number))
+
