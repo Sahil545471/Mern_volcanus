81b7a46b7c5799e167d6d95a1b261854e12dddcc
sahil545471 <c.sahil545471@gmail.com>
2024-02-07 12:55:57 +0530

    javascript basic

---
+++ day31_js_basic/script.js
@@ -0,0 +1,89 @@
+// // const age = 18
+
+// const age = prompt("Enter you age")
+
+// console.log("user entered age",age)
+
+// if(age>18)
+// {
+//     const city = prompt("ENter city")
+
+//     if(city != "indore" )
+//     {
+//         console.log("YOU are not eligible")
+//     }
+//     else{
+
+//         console.log("you can drive")
+//     }
+// }
+// else{
+//     console.log("you can not drive")
+// }
+
+// const $dollar = 500;
+
+// if($dollar >= 500)
+// {
+    //     console.log("You can purchase iphone")
+    // }
+    // else{
+        //     console.log("you have to sale your kidney")
+        // }
+        
+//         const $dollar = 500;
+// (10<$dollar) ? (console.log("true")) : (console.log("false"))
+
+// switch case 
+
+// const day = parseInt(prompt("Enter the day"));
+
+
+// switch(day)
+// {
+//     case 1:
+//     console.log("Monday");
+//     break;
+
+//     case 2:
+//     console.log("Tuesdya");
+//     break;
+
+//     case 3:
+//     console.log("wednesday");
+//     break;
+
+//     case 4:
+//     console.log("thursday");
+//     break;
+
+//     case 5:
+//     console.log("friday")
+//     break;
+
+//     case 6:
+//     console.log("saturday")
+//     break;
+
+//     case 7:
+//     console.log("sunday")
+//     break;
+
+//     default:
+//     console.log("Invalid input");
+
+// }
+
+// Normal function declaration
+function print(){
+    console.log("This is a function body")
+}
+// print();
+
+// Function with return value
+function pi(){
+    return "JAI SHREE RAM"
+}
+const result = pi()
+// console.log(result)
+console.log(pi())
