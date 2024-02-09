51ff237a9d626ad5735bfa126a5c0e1e225f088a
sahil545471 <c.sahil545471@gmail.com>
2024-02-09 12:07:20 +0530

    loops of javascript

---
+++ day33_javascript/script.js
@@ -0,0 +1,51 @@
+// const arr = [10,20,"superman","spiderman",2098.2,"a"]
+
+// for(let i = 0; i<arr.length; i++)
+// {
+//     console.log(arr[i])
+// }
+
+// let i=0
+// while(i<arr.length)
+// {
+//     console.log(arr[i])
+//     i++
+// }
+// do{
+//     console.log(arr[i])
+//     i++
+// }while(i<arr.length)
+
+// foreach , map , fillter
+// const arr = [10,20,"superman","spiderman",2098.2,"a","hello"]
+// for in loop
+// for(let x in arr){
+//     console.log("The index "+x+ " value",arr[x]);
+// }
+
+
+// for of loop
+// for(let x of arr)
+// {
+//     console.log(x);
+// }
+
+
+// const person = {
+//     Name: "Gourav",
+//     surname:"Pardesi",
+//     age:"22",
+//     gender:"male",
+//     work:"pta nhi",
+//     job:"dhundh rhe h",
+//     Education:"Ho nhi rhi",
+// }
+
+// for(let x in person){
+//     console.log(x, person[x])
+// }
+
+// For each loop
+const arr = [10,20,"superman","spiderman",2098.2,"a","hello"]
+arr.forEach((value,index,array) =>console.log (value,index,array))
+
