e91d14cb0deff81f2fdda0d040a9d24f167cab01
sahil545471 <c.sahil545471@gmail.com>
2024-02-13 13:01:33 +0530

    Array in java

---
+++ day_34_javascript/script.js
@@ -0,0 +1,29 @@
+//  let heros = ["superman","spiderman","batman","hulk",2012,21485.21]
+
+//  let arr = [] 
+
+//  arr[0] = "ram"
+//  arr[1] = "sham"
+
+// let arrobj = [
+//     {name:"ram",gav:"indore",age:"50000"},
+//     {name:"sahil",gav:"indore",age:"8000"},
+//     {name:"anurag",gav:"resere",age:"50"},
+//     {name:"adewva",gav:"rere",age:"512c0"}
+// ]
+
+// console.log(arrobj)
+//  console.log(heros[1])
+//  console.log(arr)
+
+let arr = [110,20,30,40]
+
+console.log("oringmal array",arr)
+
+// arr.push("apple") 
+// console.log("popelement is = ",arr.pop())
+// arr.shift()
+// console.log("removing first element ",arr.shift())
+console.log(arr.unshift("don","shaktiman","1203"))
+
+console.log(" modiofireed oringmal array",arr)
